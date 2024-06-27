45269e37311e52fce17e71a5cf5254fdddb6619d
NewGen2022 <amirgimari@gmail.com>
2024-06-27 20:49:15 +0300

    added info clarifying

--- src/game/Info.jsx
+++ src/game/Info.jsx
@@ -8,9 +8,11 @@ const Info = ({ setCloseInfo }) => {
 
     return (
         <div id="info">
-            There is 3 levels: EASY, MEDIUM and HARD.
-            After you complete the one, it will automatically change 
-            the difficulty to next. After you complete HARD level you win.
+            There is 3 levels: EASY (3 points needed), MEDIUM(5) and HARD(7).
+            After you complete the one (get as many points as you need for each level), 
+            it will automatically change the difficulty to next. 
+            After you complete HARD level you win.
+            <p className='help-info'>DO NOT CLICK ON THE SAME CARD TWICE IN THE ONE LEVEL</p>
             <span>GOOD LUCK!</span>
             <button className="close-info-btn" onClick={handleCloseInfo}>Close</button>
         </div>
